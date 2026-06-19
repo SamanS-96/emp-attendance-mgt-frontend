d8fc4486759476656363356719777d8bb8ecb154
SamanS-96 <samanpsumanasiri@gmail.com>
2026-06-19 07:02:02 +0530

    routing done for employee page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Employee } from './pages/employee/employee';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'employee',
+    component: Employee
+  }
+];
