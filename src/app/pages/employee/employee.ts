82b34e26f76ba72f76190223182465780ac7a663
SamanS-96 <samanpsumanasiri@gmail.com>
2026-06-19 08:09:05 +0530

    show employees detail on browser with install Angular Material

--- src/app/pages/employee/employee.ts
+++ src/app/pages/employee/employee.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../services/employee';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [
+    MatTableModule
+  ],
   templateUrl: './employee.html',
   styleUrl: './employee.css',
 })
@@ -11,22 +14,31 @@ export class Employee implements OnInit {
 
   employees: any[] = [];
 
+  displayedColumns: string[] = [
+    'code',
+    'firstName',
+    'email'
+  ];
+
   constructor(
     private employeeService: EmployeeService
   ) {}
 
   ngOnInit(): void {
 
-    this.employeeService.getAll()
-      .subscribe({
-        next: (data: any) => {
-          console.log('SUCCESS', data);
-          this.employees = data;
-        },
-        error: (error) => {
-          console.log('ERROR', error);
-        }
-      });
+  this.employeeService.getAll()
+    .subscribe({
+      next: (data:any)=>{
 
-  }
+        console.log("EMP DATA :", data);
+
+        this.employees = data;
+
+      },
+      error:(error)=>{
+        console.log(error);
+      }
+    });
+
+}
 }
