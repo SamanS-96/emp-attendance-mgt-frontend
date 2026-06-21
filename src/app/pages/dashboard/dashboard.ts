137533619f610bde65513e6771b757f047be7610
SamanS-96 <samanpsumanasiri@gmail.com>
2026-06-21 12:51:41 +0530

    dasboard & side panel design with CSS

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+
+  currentUser: any;
   
-  constructor() {}
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.authService.getUser();
+  }
 
 }
